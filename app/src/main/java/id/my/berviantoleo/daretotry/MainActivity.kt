1e536ca2a0fc9abe3fee2e96223da88378b883d4
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2020-03-12 07:30:34 +0700

    Initial Project with Sign In UI
    Signed-off-by: Bervianto Leo Pratama <bervianto.leo@gmail.com>

---
+++ app/src/main/java/id/my/berviantoleo/daretotry/MainActivity.kt
@@ -0,0 +1,72 @@
+package id.my.berviantoleo.daretotry
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    private val RC_SIGN_IN: Int = 10
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        signInButton.setOnClickListener {
+            runSignIn()
+        }
+        if (FirebaseAuth.getInstance().currentUser != null) {
+            runDashboard()
+        } else {
+            runSignIn()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == RC_SIGN_IN) {
+            val response = IdpResponse.fromResultIntent(data)
+
+            if (resultCode == Activity.RESULT_OK) {
+                // Successfully signed in
+                val user = FirebaseAuth.getInstance().currentUser
+                Log.i("MainActivity", "User email: ${user?.email}")
+                runDashboard()
+                // ...
+            } else {
+                // Sign in failed. If response is null the user canceled the
+                // sign-in flow using the back button. Otherwise check
+                // response.getError().getErrorCode() and handle the error.
+                // ...
+
+            }
+        }
+    }
+
+    private fun runSignIn() {
+        val providers = arrayListOf(
+            AuthUI.IdpConfig.EmailBuilder().build(),
+            AuthUI.IdpConfig.PhoneBuilder().build(),
+            AuthUI.IdpConfig.GoogleBuilder().build()
+        )
+        // Create and launch sign-in intent
+        startActivityForResult(
+            AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(providers)
+                .build(),
+            RC_SIGN_IN
+        )
+    }
+
+    private fun runDashboard() {
+        val intent = Intent(this, DashboardActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+}
